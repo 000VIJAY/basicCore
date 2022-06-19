2136c66f6450fdb0b62e987da38cdde0018614a6
000VIJAY <vijay893689@gmail.com>
2022-06-19 13:43:02 +0530

    Added Greatest of three number

--- Program.cs
+++ Program.cs
@@ -1,24 +1,23 @@
-﻿Console.WriteLine("write the character");
-char word = Convert.ToChar(Console.ReadLine());
-
-switch (word)
+﻿Console.WriteLine("Write three numbers");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("num1 : " + num1);
+Console.WriteLine("num2 : " + num2);
+Console.WriteLine("num3 : " + num3);
+if (num1 > num2 && num1 > num3)
 {
-    case 'a':
-        Console.WriteLine("The alphabet is vowel");
-        break;
-    case 'e':
-        Console.WriteLine("The alphabet is vowel");
-        break;
-    case 'i':
-        Console.WriteLine("The alphabet is vowel");
-        break;
-    case 'o':
-        Console.WriteLine("The alphabet is vowel");
-        break;
-    case 'u':
-        Console.WriteLine("The alphabet is vowel");
-        break;
-    default:
-        Console.WriteLine("The alphabet is consonant");
-        break;
+    Console.WriteLine("Greatest Number : " + num1);
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine("Greatest number : " + num2);
+}
+else if (num3 > num1 && num3 > num2)
+{
+    Console.WriteLine("Greatest Number : " + num3);
+}
+else
+{
+    Console.WriteLine("All three numbers are equal");
 }

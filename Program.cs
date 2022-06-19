bbfd7f89025bdd6ef5d1b6e55e481f0c15a54e26
000VIJAY <vijay893689@gmail.com>
2022-06-19 13:48:20 +0530

    added Program to find Harmonic value

--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
-﻿Console.WriteLine("Write three numbers");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
-int num3 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("num1 : " + num1);
-Console.WriteLine("num2 : " + num2);
-Console.WriteLine("num3 : " + num3);
-if (num1 > num2 && num1 > num3)
+﻿Console.WriteLine("Write Number whose Harmonic value wants to know");
+int num = Convert.ToInt32(Console.ReadLine());
+double numOne = 1;
+for (int i = 2; i <= num; i++)
 {
-    Console.WriteLine("Greatest Number : " + num1);
+    numOne = numOne + 1.0 / i;
 }
-else if (num2 > num1 && num2 > num3)
-{
-    Console.WriteLine("Greatest number : " + num2);
-}
-else if (num3 > num1 && num3 > num2)
-{
-    Console.WriteLine("Greatest Number : " + num3);
-}
-else
-{
-    Console.WriteLine("All three numbers are equal");
-}
+Console.WriteLine("Harmonic value : " + numOne);
+
